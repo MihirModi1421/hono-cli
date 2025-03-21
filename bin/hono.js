b89cfa30c9ff4679757e23f3b59345f6e6c97f5d
MihirModi <mihirmodi1421@gmail.com>
2025-03-21 17:55:44 +0530

    [inquirer] add inquirer

--- bin/hono.js
+++ bin/hono.js
@@ -2,15 +2,22 @@
 
 import { program } from "commander";
 import chalk from "chalk";
+import inquirer from "inquirer";
 
-program
-  .version("1.0.0")
-  .description("My Node CLI")
-  .option("-n, --name <type>", "Add your name")
-  .action((options) => {
-    console.log(chalk.blue(`Hey, ${options.name}!`));
-    console.log(chalk.green(`Hey, ${options.name}!`));
-    console.log(chalk.red(`Hey, ${options.name}!`));
-  });
+program.version("1.0.0").description("My Node CLI");
+
+program.action(() => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What's your name?",
+      },
+    ])
+    .then((answers) => {
+      console.log(chalk.green(`Hey there, ${answers.name}!`));
+    });
+});
 
 program.parse(process.argv);
